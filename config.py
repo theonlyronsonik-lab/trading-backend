498802e30085704e301e0c8c9cb0ba53b2e274a8
theonlyronsonik-lab <theonlyronsonik@gmail.com>
2025-12-19 20:49:35 +0300

    Add risk management settings to config.py
    Added risk management parameters for trading.

--- config.py
+++ config.py
@@ -13,3 +13,8 @@ TELEGRAM_CHAT_ID = "PASTE_YOUR_CHAT_ID"
 # ===== API =====
 API_KEY = "PASTE_YOUR_API_KEY"
 BASE_URL = "https://api.twelvedata.com"
+# ===== RISK MANAGEMENT =====
+ACCOUNT_BALANCE = 1000     # change later
+RISK_PERCENT = 1           # % per trade
+RR_RATIO = 2               # Risk : Reward
+
