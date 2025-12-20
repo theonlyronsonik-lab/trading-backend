39faee4b8e8dda3e2359ef34d62f2e043d0b0f7e
theonlyronsonik-lab <theonlyronsonik@gmail.com>
2025-12-20 09:19:00 +0300

    Delete session handling functions
    Remove session handling functions from sessions.py

--- sessions.py
+++ sessions.py
@@ -1,16 +1,1 @@
-from datetime import datetime
 
-def get_session(timestamp):
-    hour = int(timestamp.split(" ")[1].split(":")[0])
-
-    if 0 <= hour < 7:
-        return "ASIA"
-    elif 7 <= hour < 15:
-        return "LONDON"
-    elif 15 <= hour < 22:
-        return "NEW_YORK"
-    else:
-        return "OFF"
-
-def session_valid(session):
-    return session in ["LONDON", "NEW_YORK"]
