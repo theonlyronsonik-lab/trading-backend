af2bc1cb52837a19a573220ac4221932a5a44fb1
theonlyronsonik-lab <theonlyronsonik@gmail.com>
2025-12-20 09:18:29 +0300

    Remove send_message function from telegram.py

--- telegram.py
+++ telegram.py
@@ -1,10 +1,1 @@
-import requests
-from config import TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID
 
-def send_message(text):
-    url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage"
-    payload = {
-        "chat_id": TELEGRAM_CHAT_ID,
-        "text": text
-    }
-    requests.post(url, json=payload)
