0f4546a4c5679866917867e7e88ed7681f31b3ba
theonlyronsonik-lab <theonlyronsonik@gmail.com>
2025-12-23 23:16:42 +0300

    Delete notifier.py

--- notifier.py
+++
@@ -1,7 +0,0 @@
-import requests
-from config import TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID
-
-def send_alert(message):
-    url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage"
-    data = {"chat_id": TELEGRAM_CHAT_ID, "text": message}
-    requests.post(url, data=data)
