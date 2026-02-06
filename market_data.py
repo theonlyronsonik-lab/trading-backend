8e8913b88401cc0913823d30f37b206bb40f3e48
theonlyronsonik-lab <theonlyronsonik@gmail.com>
2026-02-06 14:27:51 +0300

    Update market_data.py

--- market_data.py
+++ market_data.py
@@ -1,21 +1,16 @@
-# market_data.py
-
 import requests
-from config import MARKET_API_KEY
+from config import MARKET_API_KEY, SYMBOLS
 
-def fetch_candles(symbol, timeframe, limit=200):
-    url = f"https://api.marketdata.com/candles"
-    params = {
-        "symbol": symbol,
-        "timeframe": timeframe,
-        "limit": limit,
-        "apikey": MARKET_API_KEY
-    }
-    
-    response = requests.get(url, params=params)
-    
+def fetch_candles(symbol, timeframe, limit):
+    url = f"https://api.twelvedata.com/time_series?symbol={symbol}&interval={timeframe}&apikey={MARKET_API_KEY}&limit={limit}"
+    response = requests.get(url)
     if response.status_code == 200:
-        return response.json()['candles']
+        data = response.json()
+        if "values" in data:
+            return data["values"]
+        else:
+            print(f"Error: No data found for {symbol}.")
+            return None
     else:
         print(f"Error fetching {symbol} candles: {response.text}")
         return None
