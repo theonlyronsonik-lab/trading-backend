dcdbf29dccfe61fc9dd9c4881bba82bdae304ec9
theonlyronsonik-lab <theonlyronsonik@gmail.com>
2026-02-09 21:40:04 +0300

    Update entry.py

--- entry.py
+++ entry.py
@@ -1,21 +1,4 @@
-def confirm_entry(bias, entry_price, sl, tp):
-    """
-    Returns True only if RR >= 1:3
-    """
-
-    risk = abs(entry_price - sl)
-    reward = abs(tp - entry_price)
-
-    if risk == 0:
-        return False
-
-    rr = reward / risk
-
-    if rr >= 3:
-        return True
-
-    return False
-    def find_ltf_entry(candles, bias):
+  def find_ltf_entry(candles, bias):
         """
         Returns:
         {
