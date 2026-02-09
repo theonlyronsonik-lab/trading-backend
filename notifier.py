ede227e8c87f571f1934436907dbc4ff43c1812a
theonlyronsonik-lab <theonlyronsonik@gmail.com>
2026-02-09 20:39:32 +0300

    Create notifier.py

---
+++ notifier.py
@@ -0,0 +1,20 @@
+import requests
+from config import TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID
+
+
+def send_message(text):
+    """
+    Sends a message to Telegram
+    """
+    url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage"
+
+    payload = {
+        "chat_id": TELEGRAM_CHAT_ID,
+        "text": text,
+        "parse_mode": "Markdown"
+    }
+
+    response = requests.post(url, json=payload, timeout=10)
+
+    if response.status_code != 200:
+        print("Telegram error:", response.text)
