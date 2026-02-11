4a27ce3e1d7a0428b069916eebdb9916e71c3c8a
theonlyronsonik-lab <theonlyronsonik@gmail.com>
2026-02-11 18:37:42 +0300

    Update config.py

--- config.py
+++ config.py
@@ -3,13 +3,13 @@ TELEGRAM_BOT_TOKEN = "8529456380:AAF2Ed2EoEtGRTfAX4a67Vd89KSnMUImdQc"
 TELEGRAM_CHAT_ID = "6599172354"
 
 # ===== TWELVE DATA =====
-TWELVE_API_KEY = "2c051684da3b481bbf6b068fbaf5767d"
+TWELVE_API_KEY = "d2849e2ab0c042edb97b8276d864a41b"
 BASE_URL = "https://api.twelvedata.com/time_series"
 
 # ===== MARKET SETTINGS =====
 SYMBOLS = ["XAU/USD", "EUR/USD", "GBP/USD","AUD/CAD"]
 
-HTF = "4h"
-LTF = "15min"
+HTF = "30min"
+LTF = "3min"
 
 LOOP_DELAY = 60  # seconds
