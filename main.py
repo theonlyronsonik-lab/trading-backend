076e5ac47113ad9aeeae90045b477124d3f4ecfc
theonlyronsonik-lab <theonlyronsonik@gmail.com>
2026-02-11 19:01:01 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from telegram_bot import send_telegram_message
 # -------------------------
 # SETTINGS
 # -------------------------
-SYMBOLS = ["GBP/USD", "EUR/USD", "AUD/CAD","XAU/USD"]
+SYMBOLS = ["GBP/USD", "EUR/USD", "AUD/CAD","SILVER", "USD/JPY"]
 
 HTF_TIMEFRAME = "30min"
 LTF_TIMEFRAME = "3min"
