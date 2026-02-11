76c201cbf847371446b86e4bfa1997f5b42cae5f
theonlyronsonik-lab <theonlyronsonik@gmail.com>
2026-02-11 22:32:51 +0300

    Update risk.py

--- risk.py
+++ risk.py
@@ -1,14 +1,10 @@
-def retest_entry(candles, bias):
-    closes = [float(c["close"]) for c in candles]
-    highs = [float(c["high"]) for c in candles]
-    lows = [float(c["low"]) for c in candles]
-
+def calculate_dynamic_sl(entry, swing, bias):
     if bias == "bullish":
-        if closes[-2] > highs[-3] and lows[-1] <= highs[-3]:
-            return float(closes[-1])
+        return swing - (entry * 0.001)
 
     if bias == "bearish":
-        if closes[-2] < lows[-3] and highs[-1] >= lows[-3]:
-            return float(closes[-1])
+        return swing + (entry * 0.001)
 
-    return None
+
+def calculate_tp(entry, sl, bias, target):
+    return target
