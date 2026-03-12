9802fe3292dcee39117ba6d8c49a9fb8b990184d
theonlyronsonik-lab <theonlyronsonik@gmail.com>
2026-03-12 07:52:55 +0300

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@
 # SYMBOLS AND TIMEFRAMES
 # -----------------------------
 SYMBOLS = ["XAU/USD", "GBP/USD","GBP/JPY", "USD/JPY", "XAU/AUD"]  # add more symbols here
-HTF = "5min"  # Higher Timeframe
+HTF = "15min"  # Higher Timeframe
 LTF = "1min"  # Lower Timeframe
 LOOP_DELAY = 30  # seconds between API calls to respect free plan limits
 
